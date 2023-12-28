3b59eebb93ac3824aa5cd85e6260d3a716a0230b
Valentin Kuznetsov <aimestereo@gmail.com>
2023-12-28 22:28:20 +0300

    Nvim plugin: add python copy relative path

--- nvim/.config/nvim/lua/custom/plugins/configs/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/configs/init.lua
@@ -36,4 +36,15 @@ return {
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   { import = 'custom.plugins.lsp' },
+
+  -- Python
+  {
+    -- TODO: investigate how useful it is
+    'ranelpadon/python-copy-reference.vim',
+    config = function()
+      vim.api.nvim_set_keymap('n', '<Space>rd', ':PythonCopyReferenceDotted<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<Space>rp', ':PythonCopyReferencePytest<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<Space>ri', ':PythonCopyReferenceImport<CR>', { noremap = true, silent = true })
+    end,
+  },
 }
