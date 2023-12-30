624c75d104b131444e1914049b0123fc0a9b29c7
Valentin Kuznetsov <aimestereo@gmail.com>
2023-12-30 20:22:25 +0300

    Kitty: fix fonts

--- interactive.sh
+++ interactive.sh
@@ -1,2 +1,9 @@
 echo "Download and install JetBrains Mono Nerd Font"
 open https://www.nerdfonts.com/font-downloads
+
+# Check that kitty know about specified font
+kitty +list-fonts --psnames | grep Nerd
+
+# verify that desired font is used by kitty
+kitty --debug-font-fallback
+
