c500ac45118f9a0c36f056f6da82c5d97576a786
Valentin Kuznetsov <aimestereo@gmail.com>
2024-01-10 19:24:35 +0300

    nvim: customize treesj keymaps

--- nvim/.config/nvim/lua/custom/plugins/configs/treesj.lua
+++ nvim/.config/nvim/lua/custom/plugins/configs/treesj.lua
@@ -2,10 +2,20 @@ return {
   -- splitting/joining blocks of code like arrays, hashes, statements, objects, dictionaries, etc.
   "Wansmer/treesj",
   event = "VeryLazy",
-  keys = { "<space>m" }, -- only hotkey for toggle
+  -- keys = { "<space>m", "<space>j", "<space>s" },
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   config = function()
-    require("treesj").setup({ --[[ your config ]]
+    local treesj = require("treesj")
+
+    treesj.setup({ --[[ your config ]]
+      use_default_keymaps = false, -- only need one key
     })
+
+    -- For use default preset and it work with dot
+    vim.keymap.set("n", "<leader>m", treesj.toggle)
+    -- For extending default preset with `recursive = true`, but this doesn't work with dot
+    vim.keymap.set("n", "<leader>M", function()
+      treesj.toggle({ split = { recursive = true } })
+    end)
   end,
 }
