a051fc79d64ad3cb55c675fc059cc1c3a9850c3d
Valentin Kuznetsov <aimestereo@gmail.com>
2024-02-12 20:06:08 +0300

    nvim: fix phantom tab jump

--- nvim/.config/nvim/lua/custom/plugins/after/nvim-cmp.lua
+++ nvim/.config/nvim/lua/custom/plugins/after/nvim-cmp.lua
@@ -20,7 +20,7 @@ cmp.setup({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ["<C-e>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
@@ -29,15 +29,15 @@ cmp.setup({
         fallback()
       end
     end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
+    --   if cmp.visible() then
+    --     cmp.select_prev_item()
+    --   elseif luasnip.jumpable(-1) then
+    --     luasnip.jump(-1)
+    --   else
+    --     fallback()
+    --   end
+    -- end, { "i", "s" }),
   }),
   sources = {
     { name = "codeium" },
