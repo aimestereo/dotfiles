badb20edff6387ea440f9efe8bf8e0cb6be9c6e7
Valentin Kuznetsov <aimestereo@gmail.com>
2024-03-02 20:24:01 +0300

    nvim: set C-y as confirm for cmp

--- nvim/.config/nvim/lua/custom/plugins/after/nvim-cmp.lua
+++ nvim/.config/nvim/lua/custom/plugins/after/nvim-cmp.lua
@@ -10,13 +10,14 @@ cmp.setup({
       luasnip.lsp_expand(args.body)
     end,
   },
+
   mapping = cmp.mapping.preset.insert({
     ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete({}),
-    ["<CR>"] = cmp.mapping.confirm({
+    ["<C-y>"] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     }),
