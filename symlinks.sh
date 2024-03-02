8f3aa8dbfb3ef6a60bc25a5fbcd3060ea83493c7
Valentin Kuznetsov <aimestereo@gmail.com>
2024-03-03 02:27:46 +0300

    stow: hide warnings

--- symlinks.sh
+++ symlinks.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+function stow() {
+    bash -c "stow $* 2> >(grep -v 'BUG in find_stowed_path? Absolute/relative mismatch' 1>&2)"
+}
+
 for d in `ls -d */`;
 do
     ( stow --restow -t ${HOME} -v $d )
