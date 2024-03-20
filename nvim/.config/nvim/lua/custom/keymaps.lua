df7bcf542130d7438575c3feb42d35461ff6f4b9
Valentin Kuznetsov <aimestereo@gmail.com>
2024-03-20 16:24:16 +0300

    nvim: update keymaps helper function

--- nvim/.config/nvim/lua/custom/keymaps.lua
+++ nvim/.config/nvim/lua/custom/keymaps.lua
@@ -1,52 +1,49 @@
-function Map(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
-  if opts then
-    options = vim.tbl_extend("force", options, opts)
-  end
-  vim.keymap.set(mode, lhs, rhs, options)
+local function map(mode, l, r, opts)
+  opts = opts or {}
+  vim.keymap.set(mode, l, r, opts)
 end
 
 --
 -- Buffers
 --
 -- close current buffer without closing split (by moving first to previous buffer)
-vim.keymap.set({ "n" }, "<A-x>", ":bp|bd #<cr>", { desc = "[C]lose current buffer" })
-vim.keymap.set({ "n" }, "<A-s>", ":wa<cr>", { desc = "[S]ave all changed files" })
-vim.keymap.set({ "i" }, "<A-s>", "<esc>:wa<cr>", { desc = "[S]ave all changed files" })
-vim.keymap.set({ "n" }, "<A-g>", ":0G<cr>", { desc = "Open [G]it fugitive in current pane." })
+map({ "n" }, "<A-x>", ":bp|bd #<cr>", { desc = "[C]lose current buffer" })
+map({ "n" }, "<A-s>", ":wa<cr>", { desc = "[S]ave all changed files" })
+map({ "i" }, "<A-s>", "<esc>:wa<cr>", { desc = "[S]ave all changed files" })
+map({ "n" }, "<A-g>", ":0G<cr>", { desc = "Open [G]it fugitive in current pane." })
 
 --
 -- Splits
 --
---vim.keymap.set({ "n" }, "<A-w>", "<C-w>", { desc = "[W]indow (Splits) manager" })
---vim.keymap.set({ "n" }, "<A-w>", ":echo 'hi'", { desc = "[W]indow (Splits) manager" })
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "[W]indow (Splits) manager", noremap = true })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "[W]indow (Splits) manager", noremap = true })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "[W]indow (Splits) manager", noremap = true })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "[W]indow (Splits) manager", noremap = true })
+--map({ "n" }, "<A-w>", "<C-w>", { desc = "[W]indow (Splits) manager" })
+--map({ "n" }, "<A-w>", ":echo 'hi'", { desc = "[W]indow (Splits) manager" })
+map("n", "<C-h>", "<C-w>h", { desc = "[W]indow (Splits) manager" })
+map("n", "<C-j>", "<C-w>j", { desc = "[W]indow (Splits) manager" })
+map("n", "<C-k>", "<C-w>k", { desc = "[W]indow (Splits) manager" })
+map("n", "<C-l>", "<C-w>l", { desc = "[W]indow (Splits) manager" })
 
-Map("n", "<A-Up>", ":resize +2<CR>", { desc = "Resize Window (Split)" })
-Map("n", "<A-Down>", ":resize -2<CR>", { desc = "Resize Window (Split)" })
-Map("n", "<A-Left>", ":vertical resize -2<CR>", { desc = "Resize Window (Split)" })
-Map("n", "<A-Right>", ":vertical resize +2<CR>", { desc = "Resize Window (Split)" })
+map("n", "<A-Up>", ":resize +2<CR>", { desc = "Resize Window (Split)" })
+map("n", "<A-Down>", ":resize -2<CR>", { desc = "Resize Window (Split)" })
+map("n", "<A-Left>", ":vertical resize -2<CR>", { desc = "Resize Window (Split)" })
+map("n", "<A-Right>", ":vertical resize +2<CR>", { desc = "Resize Window (Split)" })
 
 --
 -- Helix or inspired by it
 --
---vim.keymap.set('n', 'd', 'x', { desc = "Delete symbol under cursor" })
-vim.keymap.set("n", "U", "<C-r>", { desc = "Redo (Reverse [U]ndo)" })
-vim.keymap.set("v", "<A-;>", "o", { desc = "Flips selection." })
+--map('n', 'd', 'x', { desc = "Delete symbol under cursor" })
+map("n", "U", "<C-r>", { desc = "Redo (Reverse [U]ndo)" })
+map("v", "<A-;>", "o", { desc = "Flips selection." })
 
-vim.keymap.set("n", "<A-c>", '"_cl', { desc = "[C]hange symbol/selection avoiding yank" })
-vim.keymap.set("n", "<A-d>", '"_dl', { desc = "[D]elete symbol/selection avoiding yank" })
-vim.keymap.set("v", "<A-c>", '"_c', { desc = "[C]hange symbol/selection avoiding yank" })
-vim.keymap.set("v", "<A-d>", '"_d', { desc = "[D]elete symbol/selection avoiding yank" })
+map("n", "<A-c>", '"_cl', { desc = "[C]hange symbol/selection avoiding yank" })
+map("n", "<A-d>", '"_dl', { desc = "[D]elete symbol/selection avoiding yank" })
+map("v", "<A-c>", '"_c', { desc = "[C]hange symbol/selection avoiding yank" })
+map("v", "<A-d>", '"_d', { desc = "[D]elete symbol/selection avoiding yank" })
 
-vim.keymap.set("x", "<A-p>", [["_dP]], { desc = "[P]aste without overwriting register" })
-vim.keymap.set("v", "<leader>p", [["_d"+P]], { desc = "[P]aste clipboard without overwriting register" })
+map("x", "<A-p>", [["_dP]], { desc = "[P]aste without overwriting register" })
+map("v", "<leader>p", [["_d"+P]], { desc = "[P]aste clipboard without overwriting register" })
 
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "[Y]ank to system clipboard" })
-vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "[Y]ank current line to system clipboard" })
+map({ "n", "v" }, "<leader>y", [["+y]], { desc = "[Y]ank to system clipboard" })
+map("n", "<leader>Y", [["+Y]], { desc = "[Y]ank current line to system clipboard" })
 
 --
 -- Refactor
@@ -68,40 +65,40 @@ vim.api.nvim_set_keymap(
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+map({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
 
 --
 -- from ThePrimeagen: https://www.youtube.com/watch?v=w7i4amO_zaE&t=38s&ab_channel=ThePrimeagen
 --
-vim.keymap.set("n", "<C-e>", vim.cmd.Ex, { desc = "Open file [E]xplorer / vim folder view containing current file" })
+map("n", "<C-e>", vim.cmd.Ex, { desc = "Open file [E]xplorer / vim folder view containing current file" })
 
 -- Move selected line / block of text in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down in visual mode" })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up in visual mode" })
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down in visual mode" })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up in visual mode" })
 
 -- Concatenate with next line and keep cursor at the same position
-vim.keymap.set("n", "J", "mzJ`z", { desc = "Concatenate with next line and keep cursor at the same position" })
+map("n", "J", "mzJ`z", { desc = "Concatenate with next line and keep cursor at the same position" })
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll [d]own half-page and center cursor" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll [u]p half-page and center cursor" })
+map("n", "<C-d>", "<C-d>zz", { desc = "Scroll [d]own half-page and center cursor" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Scroll [u]p half-page and center cursor" })
 
-vim.keymap.set("n", "n", "nzzzv", { desc = "Go to a [n]ext match, keep cursor at the center of the screen" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "Go to a previous match, keep cursor at the center of the screen" })
+map("n", "n", "nzzzv", { desc = "Go to a [n]ext match, keep cursor at the center of the screen" })
+map("n", "N", "Nzzzv", { desc = "Go to a previous match, keep cursor at the center of the screen" })
 
-vim.keymap.set("i", "<C-c>", "<Esc>", { desc = "Esc" })
-vim.keymap.set("n", "Q", "<nop>", { desc = "Silence Q" })
+map("i", "<C-c>", "<Esc>", { desc = "Esc" })
+map("n", "Q", "<nop>", { desc = "Silence Q" })
 
-vim.keymap.set(
+map(
   "n",
   "<leader>sr",
   [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
   { desc = "[S]earch and [R]eplace (with regex over whole file) currenft word" }
 )
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make current file e[x]ecutable" })
+map("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "Make current file e[x]ecutable" })
 
 -- Tabs
-vim.keymap.set("n", "<leader><S-Tab>", ":set invlist<CR>", { desc = "Show tabs" })
+map("n", "<leader><S-Tab>", ":set invlist<CR>", { desc = "Show tabs" })
--- nvim/.config/nvim/lua/custom/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/plugins/lsp/lspconfig.lua
@@ -28,11 +28,12 @@ return {
 
     local servers = require("custom.plugins.lsp.consts.servers")
 
-    local keymap = vim.keymap -- for conciseness
-
-    local opts = { noremap = true, silent = true }
     local on_attach = function(client, bufnr)
-      opts.buffer = bufnr
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
 
       if client.name == "ruff_lsp" then
         -- Disable hover in favor of Pyright
@@ -40,69 +41,43 @@ return {
       end
 
       -- set keybinds
-      opts.desc = "Show LSP references"
-      keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+      map("n", "gR", "<cmd>Telescope lsp_references<CR>", { desc = "Show LSP references" }) -- show definition, references
+      map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" }) -- go to declaration
+      map("n", "gd", "<cmd>Telescope lsp_definitions<CR>", { desc = "Show LSP definitions" }) -- show lsp definitions
+      map("n", "gi", "<cmd>Telescope lsp_implementations<CR>", { desc = "Show LSP implementations" }) -- show lsp implementations
+      map("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", { desc = "Show LSP type definitions" }) -- show lsp type definitions
 
-      opts.desc = "Go to declaration"
-      keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+      map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "See available code actions" }) -- see available code actions, in visual mode will apply to selection
 
-      opts.desc = "Show LSP definitions"
-      keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-
-      opts.desc = "Show LSP implementations"
-      keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-
-      opts.desc = "Show LSP type definitions"
-      keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
-      opts.desc = "See available code actions"
-      keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-      opts.desc = "Smart rename"
-      keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+      map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Smart rename" }) -- smart rename
 
       -- Documentation
 
-      opts.desc = "Show documentation for what is under cursor"
-      keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-      opts.desc = "Show signature documentation"
-      keymap.set("n", "L", vim.lsp.buf.signature_help, opts)
+      map("n", "K", vim.lsp.buf.hover, { desc = "Show documentation for what is under cursor" }) -- show documentation for what is under cursor
+      map("n", "L", vim.lsp.buf.signature_help, { desc = "Show signature documentation" })
 
       -- Symbols
 
-      opts.desc = "[D]ocument [S]ymbols"
-      keymap.set("n", "<leader>ds", require("telescope.builtin").lsp_document_symbols, opts)
-
-      opts.desc = "[W]orkspace [S]ymbols"
-      keymap.set("n", "<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, opts)
+      map("n", "<leader>ds", require("telescope.builtin").lsp_document_symbols, { desc = "[D]ocument [S]ymbols" })
+      map(
+        "n",
+        "<leader>ws",
+        require("telescope.builtin").lsp_dynamic_workspace_symbols,
+        { desc = "[W]orkspace [S]ymbols" }
+      )
 
       -- Diagnostics
 
-      opts.desc = "[T]oggle [D]iagnostics"
-      keymap.set("n", "<leader>dtd", ":DiagnosticsToggle<CR>", opts)
-
-      opts.desc = "[T]oggle [D]iagnostics [I]nline text"
-      keymap.set("n", "<leader>dti", ":DiagnosticsToggleVirtualText<CR>", opts)
-
-      opts.desc = "Show [D]ocument [D]iagnostics"
-      keymap.set("n", "<leader>dd", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-      opts.desc = "Show [D]iagnostics per [L]ine"
-      keymap.set("n", "<leader>dl", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-      opts.desc = "Go to previous diagnostic"
-      keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-      opts.desc = "Go to next diagnostic"
-      keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+      map("n", "<leader>dtd", ":DiagnosticsToggle<CR>", { desc = "[T]oggle [D]iagnostics" })
+      map("n", "<leader>dti", ":DiagnosticsToggleVirtualText<CR>", { desc = "[T]oggle [D]iagnostics [I]nline text" })
+      map("n", "<leader>dd", "<cmd>Telescope diagnostics bufnr=0<CR>", { desc = "Show [D]ocument [D]iagnostics" }) -- show  diagnostics for file
+      map("n", "<leader>dl", vim.diagnostic.open_float, { desc = "Show [D]iagnostics per [L]ine" }) -- show diagnostics for line
+      map("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" }) -- jump to previous diagnostic in buffer
+      map("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" }) -- jump to next diagnostic in buffer
 
       -- See formatting.lua
-      -- opts.desc = '[F]ormat'
-      -- keymap.set('n', '<leader>f', vim.lsp.buf.format, opts)
-
-      opts.desc = "Restart LSP"
-      keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+      -- map('n', '<leader>f', vim.lsp.buf.format, { desc = '[F]ormat' }) -- set in conform
+      map("n", "<leader>rs", ":LspRestart<CR>", { desc = "Restart LSP" }) -- mapping to restart lsp if necessary
     end
 
     -- used to enable autocompletion (assign to every lsp server config)
