c1efb7a9547bea8c842d68ee2875d7044dc99b44
Valentin Kuznetsov <aimestereo@gmail.com>
2024-03-20 16:06:05 +0300

    nvim: add lspkind for cmp icons

--- nvim/.config/nvim/lua/custom/plugins/after/nvim-cmp.lua
+++ nvim/.config/nvim/lua/custom/plugins/after/nvim-cmp.lua
@@ -4,6 +4,8 @@ local luasnip = require("luasnip")
 require("luasnip.loaders.from_vscode").lazy_load()
 luasnip.config.setup({})
 
+local lspkind = require("lspkind")
+
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -27,5 +29,46 @@ cmp.setup({
     { name = "copilot" },
     { name = "luasnip" },
     { name = "nvim_lsp" },
+  },
+
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = "symbol", -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      -- can also be a function to dynamically calculate max width such as
+      -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+
+      symbol_map = {
+        Copilot = "",
+        -- defaults:
+        Text = "󰉿",
+        Method = "󰆧",
+        Function = "󰊕",
+        Constructor = "",
+        Field = "󰜢",
+        Variable = "󰀫",
+        Class = "󰠱",
+        Interface = "",
+        Module = "",
+        Property = "󰜢",
+        Unit = "󰑭",
+        Value = "󰎠",
+        Enum = "",
+        Keyword = "󰌋",
+        Snippet = "",
+        Color = "󰏘",
+        File = "󰈙",
+        Reference = "󰈇",
+        Folder = "󰉋",
+        EnumMember = "",
+        Constant = "󰏿",
+        Struct = "󰙅",
+        Event = "",
+        Operator = "󰆕",
+        TypeParameter = "",
+      },
+      ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+    }),
   },
 })
