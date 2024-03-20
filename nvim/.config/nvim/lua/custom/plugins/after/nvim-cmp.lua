8a904580f462c481616b9d7d737a434ea4d770a5
Valentin Kuznetsov <aimestereo@gmail.com>
2024-03-20 15:38:34 +0300

    nvim: cmp: use <C-e> for abort

--- nvim/.config/nvim/lua/custom/plugins/after/nvim-cmp.lua
+++ nvim/.config/nvim/lua/custom/plugins/after/nvim-cmp.lua
@@ -17,28 +17,11 @@ cmp.setup({
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete({}),
+    ["<C-e>"] = cmp.mapping.abort(),
     ["<C-y>"] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     }),
-    ["<C-e>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_prev_item()
-    --   elseif luasnip.jumpable(-1) then
-    --     luasnip.jump(-1)
-    --   else
-    --     fallback()
-    --   end
-    -- end, { "i", "s" }),
   }),
   sources = {
     { name = "luasnip" },
