c2fafeaf1bed69d80e18200d713da3e017af6f02
Valentin Kuznetsov <aimestereo@gmail.com>
2024-05-06 22:48:53 +0300

    nvim: refactor keymaps

--- nvim/.config/nvim/lua/custom/keymaps.lua
+++ nvim/.config/nvim/lua/custom/keymaps.lua
@@ -1,49 +1,50 @@
-local function map(mode, l, r, opts)
-  opts = opts or {}
-  vim.keymap.set(mode, l, r, opts)
+function Map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.keymap.set(mode, lhs, rhs, options)
 end
 
 --
 -- Buffers
 --
 -- close current buffer without closing split (by moving first to previous buffer)
-map({ "n" }, "<A-x>", ":bp|bd #<cr>", { desc = "[C]lose current buffer" })
-map({ "n" }, "<A-s>", ":wa<cr>", { desc = "[S]ave all changed files" })
-map({ "i" }, "<A-s>", "<esc>:wa<cr>", { desc = "[S]ave all changed files" })
-map({ "n" }, "<A-g>", ":0G<cr>", { desc = "Open [G]it fugitive in current pane." })
+Map({ "n" }, "<A-x>", ":bp|bd #<cr>", { desc = "[C]lose current buffer" })
+Map({ "n" }, "<A-s>", ":wa<cr>", { desc = "[S]ave all changed files" })
+Map({ "i" }, "<A-s>", "<esc>:wa<cr>", { desc = "[S]ave all changed files" })
+Map({ "n" }, "<A-g>", ":0G<cr>", { desc = "Open [G]it fugitive in current pane." })
 
 --
 -- Splits
 --
 --map({ "n" }, "<A-w>", "<C-w>", { desc = "[W]indow (Splits) manager" })
 --map({ "n" }, "<A-w>", ":echo 'hi'", { desc = "[W]indow (Splits) manager" })
-map("n", "<C-h>", "<C-w>h", { desc = "[W]indow (Splits) manager" })
-map("n", "<C-j>", "<C-w>j", { desc = "[W]indow (Splits) manager" })
-map("n", "<C-k>", "<C-w>k", { desc = "[W]indow (Splits) manager" })
-map("n", "<C-l>", "<C-w>l", { desc = "[W]indow (Splits) manager" })
+Map("n", "<C-h>", "<C-w>h", { desc = "[W]indow (Splits) manager" })
+Map("n", "<C-j>", "<C-w>j", { desc = "[W]indow (Splits) manager" })
+Map("n", "<C-k>", "<C-w>k", { desc = "[W]indow (Splits) manager" })
+Map("n", "<C-l>", "<C-w>l", { desc = "[W]indow (Splits) manager" })
 
-map("n", "<A-Up>", ":resize +2<CR>", { desc = "Resize Window (Split)" })
-map("n", "<A-Down>", ":resize -2<CR>", { desc = "Resize Window (Split)" })
-map("n", "<A-Left>", ":vertical resize -2<CR>", { desc = "Resize Window (Split)" })
-map("n", "<A-Right>", ":vertical resize +2<CR>", { desc = "Resize Window (Split)" })
+Map("n", "<A-Up>", ":resize +2<CR>", { desc = "Resize Window (Split)" })
+Map("n", "<A-Down>", ":resize -2<CR>", { desc = "Resize Window (Split)" })
+Map("n", "<A-Left>", ":vertical resize -2<CR>", { desc = "Resize Window (Split)" })
+Map("n", "<A-Right>", ":vertical resize +2<CR>", { desc = "Resize Window (Split)" })
 
 --
 -- Helix or inspired by it
 --
---map('n', 'd', 'x', { desc = "Delete symbol under cursor" })
-map("n", "U", "<C-r>", { desc = "Redo (Reverse [U]ndo)" })
-map("v", "<A-;>", "o", { desc = "Flips selection." })
+Map("n", "U", "<C-r>", { desc = "Redo (Reverse [U]ndo)" })
 
-map("n", "<A-c>", '"_cl', { desc = "[C]hange symbol/selection avoiding yank" })
-map("n", "<A-d>", '"_dl', { desc = "[D]elete symbol/selection avoiding yank" })
-map("v", "<A-c>", '"_c', { desc = "[C]hange symbol/selection avoiding yank" })
-map("v", "<A-d>", '"_d', { desc = "[D]elete symbol/selection avoiding yank" })
+Map("n", "<A-c>", '"_cl', { desc = "[C]hange symbol/selection avoiding yank" })
+Map("n", "<A-d>", '"_dl', { desc = "[D]elete symbol/selection avoiding yank" })
+Map("v", "<A-c>", '"_c', { desc = "[C]hange symbol/selection avoiding yank" })
+Map("v", "<A-d>", '"_d', { desc = "[D]elete symbol/selection avoiding yank" })
 
-map("x", "<A-p>", [["_dP]], { desc = "[P]aste without overwriting register" })
-map("v", "<leader>p", [["_d"+P]], { desc = "[P]aste clipboard without overwriting register" })
+Map("x", "<A-p>", '"_dP', { desc = "[P]aste without overwriting register" })
+Map("v", "<leader>p", '"_d"+P', { desc = "[P]aste clipboard without overwriting register" })
 
-map({ "n", "v" }, "<leader>y", [["+y]], { desc = "[Y]ank to system clipboard" })
-map("n", "<leader>Y", [["+Y]], { desc = "[Y]ank current line to system clipboard" })
+Map({ "n", "v" }, "<leader>y", '"+y', { desc = "[Y]ank to system clipboard" })
+Map("n", "<leader>Y", '"+Y', { desc = "[Y]ank current line to system clipboard" })
 
 --
 -- Refactor
@@ -65,40 +66,40 @@ vim.api.nvim_set_keymap(
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
-map({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+Map({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Remap for dealing with word wrap
-map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
-map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+Map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+Map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
 
 --
 -- from ThePrimeagen: https://www.youtube.com/watch?v=w7i4amO_zaE&t=38s&ab_channel=ThePrimeagen
 --
-map("n", "<C-e>", vim.cmd.Ex, { desc = "Open file [E]xplorer / vim folder view containing current file" })
+Map("n", "<C-e>", vim.cmd.Ex, { desc = "Open file [E]xplorer / vim folder view containing current file" })
 
 -- Move selected line / block of text in visual mode
-map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down in visual mode" })
-map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up in visual mode" })
+Map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down in visual mode" })
+Map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up in visual mode" })
 
 -- Concatenate with next line and keep cursor at the same position
-map("n", "J", "mzJ`z", { desc = "Concatenate with next line and keep cursor at the same position" })
+Map("n", "J", "mzJ`z", { desc = "Concatenate with next line and keep cursor at the same position" })
 
-map("n", "<C-d>", "<C-d>zz", { desc = "Scroll [d]own half-page and center cursor" })
-map("n", "<C-u>", "<C-u>zz", { desc = "Scroll [u]p half-page and center cursor" })
+Map("n", "<C-d>", "<C-d>zz", { desc = "Scroll [d]own half-page and center cursor" })
+Map("n", "<C-u>", "<C-u>zz", { desc = "Scroll [u]p half-page and center cursor" })
 
-map("n", "n", "nzzzv", { desc = "Go to a [n]ext match, keep cursor at the center of the screen" })
-map("n", "N", "Nzzzv", { desc = "Go to a previous match, keep cursor at the center of the screen" })
+Map("n", "n", "nzzzv", { desc = "Go to a [n]ext match, keep cursor at the center of the screen" })
+Map("n", "N", "Nzzzv", { desc = "Go to a previous match, keep cursor at the center of the screen" })
 
-map("i", "<C-c>", "<Esc>", { desc = "Esc" })
-map("n", "Q", "<nop>", { desc = "Silence Q" })
+Map("i", "<C-c>", "<Esc>", { desc = "Esc" })
+Map("n", "Q", "<nop>", { desc = "Silence Q" })
 
-map(
+Map(
   "n",
   "<leader>sr",
   [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
   { desc = "[S]earch and [R]eplace (with regex over whole file) currenft word" }
 )
-map("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "Make current file e[x]ecutable" })
+Map("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "Make current file e[x]ecutable" })
 
 -- Tabs
-map("n", "<leader><S-Tab>", ":set invlist<CR>", { desc = "Show tabs" })
+Map("n", "<leader><S-Tab>", ":set invlist<CR>", { desc = "Show tabs" })
