ecc9678c5fc7139dc59ade413c5385c933884e1d
Valentin Kuznetsov <aimestereo@gmail.com>
2024-05-12 05:01:02 +0300

    nvim: update folding rules

--- nvim/.config/nvim/lua/custom/plugins/after/folding.lua
+++ nvim/.config/nvim/lua/custom/plugins/after/folding.lua
@@ -1,17 +1,22 @@
--- Fold code using treesitter if available
+vim.g.disable_autofolding = true
+
+local enable_folding = function()
+  if require("nvim-treesitter.parsers").has_parser() then
+    vim.opt.foldmethod = "expr"
+    vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+  else
+    -- vim.opt.foldmethod = "indent"
+    vim.opt.foldmethod = "syntax"
+  end
+end
+
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
   callback = function()
-    if require("nvim-treesitter.parsers").has_parser() then
-      vim.opt.foldmethod = "expr"
-      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-    else
-      -- vim.opt.foldmethod = "indent"
-      vim.opt.foldmethod = "syntax"
-    end
-
     if vim.g.disable_autofolding then
       -- start with all folds open
       vim.cmd("normal! zR")
+    else
+      enable_folding()
     end
   end,
 })
@@ -19,6 +24,7 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
 -- Configure auto folding
 vim.api.nvim_create_user_command("FoldingEnable", function()
   vim.g.disable_autofolding = false
+  enable_folding()
   print("Autofolding re-enabled")
 end, {
   desc = "Re-enable autofolding",
@@ -26,6 +32,7 @@ end, {
 
 vim.api.nvim_create_user_command("FoldingDisable", function()
   vim.g.disable_autofolding = true
+  vim.cmd("normal zR")
   print("Autofolding disabled")
 end, {
   desc = "Disable autofolding",
