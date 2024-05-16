6baa100ff9df2338152444e698ffc312c7151a46
Valentin Kuznetsov <aimestereo@gmail.com>
2024-05-16 17:32:30 +0300

    nvim: run python cleanup

--- nvim/.config/nvim/ftplugin/python.lua
+++ nvim/.config/nvim/ftplugin/python.lua
@@ -15,12 +15,8 @@ end
 
 local function run_curr_python_file()
   print("Running current python file")
-  local lines = GET_CURRENT_BUFFER_CONTENT(0)
   local file_name = vim.api.nvim_buf_get_name(0)
   RUN_IN_TERMINAL({ "python", file_name }, false)
-
-  -- local output = EXEC({ "!python", '"' .. file_name .. '"' })
-  -- OUTPUT_TO_BUFFER("python", lines, output)
 end
 
 local function run_lines()
@@ -28,9 +24,6 @@ local function run_lines()
   local lines = GET_SELECTED_LINES()
   vim.api.nvim_call_function("writefile", { lines, temp_file })
   RUN_IN_TERMINAL({ "python", temp_file }, false)
-
-  -- local output = EXEC({ "!python", '"' .. temp_file .. '"' })
-  -- OUTPUT_TO_BUFFER("python", lines, output)
 end
 
 -- Key mappings
