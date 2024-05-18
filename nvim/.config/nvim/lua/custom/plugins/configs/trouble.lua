23c6ebc9c0b2fb4f712f41e7970f25b227956db5
Valentin Kuznetsov <aimestereo@gmail.com>
2024-05-18 23:10:56 +0300

    nvim: delete after/trouble.lua

--- nvim/.config/nvim/lua/custom/plugins/configs/trouble.lua
+++ nvim/.config/nvim/lua/custom/plugins/configs/trouble.lua
@@ -2,4 +2,29 @@ return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {},
+  config = function()
+    Map("n", "<leader>xx", function()
+      require("trouble").toggle()
+    end, { desc = "Toggle Trouble" })
+
+    Map("n", "<leader>xw", function()
+      require("trouble").toggle("workspace_diagnostics")
+    end, { desc = "workspace diagnostics" })
+
+    Map("n", "<leader>xd", function()
+      require("trouble").toggle("document_diagnostics")
+    end, { desc = "document diagnostics" })
+
+    Map("n", "<leader>xq", function()
+      require("trouble").toggle("quickfix")
+    end, { desc = "quickfix list" })
+
+    Map("n", "<leader>xl", function()
+      require("trouble").toggle("loclist")
+    end, { desc = "loclist" })
+
+    Map("n", "<leader>xR", function()
+      require("trouble").toggle("lsp_references")
+    end, { desc = "LSP references" })
+  end,
 }
