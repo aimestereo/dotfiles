f1e8147d8daee8db4e7660d24eecb45b5e326c38
Valentin Kuznetsov <aimestereo@gmail.com>
2024-05-18 21:20:35 +0300

    fixup! nvim: add dotenv

--- nvim/.config/nvim/lua/custom/plugins/configs/dotenv.lua
+++ nvim/.config/nvim/lua/custom/plugins/configs/dotenv.lua
@@ -3,6 +3,6 @@ return {
   config = {
     -- event = "VimEnter",
     enable_on_load = true, -- will load your .env file upon loading a buffer
-    verbose = true, -- show error notification if .env file is not found and if .env is loaded
+    verbose = false, -- show error notification if .env file is not found and if .env is loaded
   },
 }
