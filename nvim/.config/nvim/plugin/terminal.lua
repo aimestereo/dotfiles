6c0bc32694d1bbc1c1d11ffeed979e22c6cfe027
Valentin Kuznetsov <aimestereo@gmail.com>
2024-05-19 19:42:12 +0300

    nvim: add terminal

---
+++ nvim/.config/nvim/plugin/terminal.lua
@@ -0,0 +1,28 @@
+--
+-- I don't plan to use Neovim native terminal
+--
+
+local set = vim.opt_local
+
+-- Set local settings for terminal buffers
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", {}),
+  callback = function()
+    set.number = false
+    set.relativenumber = false
+    set.scrolloff = 0
+  end,
+})
+
+-- Easily hit escape in terminal mode.
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
+
+-- Open a terminal at the bottom of the screen with a fixed height.
+vim.keymap.set("n", "<leader><leader>t", function()
+  vim.cmd.new()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 12)
+  vim.wo.winfixheight = true
+  vim.cmd.term()
+  vim.api.nvim_feedkeys("i", "t", false)
+end)
