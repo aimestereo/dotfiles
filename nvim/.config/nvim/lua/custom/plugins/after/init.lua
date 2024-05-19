677fa2af6eea9452de14bde61cdd59bed7ef6494
Valentin Kuznetsov <aimestereo@gmail.com>
2024-05-19 10:19:03 +0300

    nvim: move folding

--- nvim/.config/nvim/lua/custom/plugins/after/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/after/init.lua
@@ -3,7 +3,6 @@ require("custom.plugins.after.nvim-cmp")
 require("custom.plugins.after.nvim-treesitter-context")
 
 require("custom.plugins.after.dashboard")
-require("custom.plugins.after.folding")
 require("custom.plugins.after.telescope")
 require("custom.plugins.after.formatting")
 -- require("custom.plugins.after.neotest")
