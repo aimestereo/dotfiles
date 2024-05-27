4953694585a4cf9998eb5e00685cf5ebac8eae0e
Valentin Kuznetsov <aimestereo@gmail.com>
2024-05-27 23:52:55 +0300

    nvim: add git-conflict

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,5 +1,52 @@
 return {
   {
+    "akinsho/git-conflict.nvim",
+    version = "*",
+    opts = {
+      default_mappings = {
+        ours = "o",
+        theirs = "t",
+        none = "0",
+        both = "b",
+        next = "n",
+        prev = "p",
+      },
+    },
+    init = function()
+      -- override the following default highlight groups for ease of viewing of merge conflicts
+      vim.cmd("highlight DiffAdd guibg = '#405d7e'")
+      vim.cmd("highlight DiffText guibg = '#314753'")
+
+      require("git-conflict").setup({
+        {
+          default_mappings = true, -- disable buffer local mapping created by this plugin
+          default_commands = true, -- disable commands created by this plugin
+          disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
+          list_opener = "copen", -- command or function to open the conflicts list
+          highlights = { -- They must have background color, otherwise the default color will be used
+            incoming = "DiffAdd",
+            current = "DiffText",
+          },
+        },
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "GitConflictDetected",
+        callback = function()
+          vim.notify("Conflict detected in file " .. vim.api.nvim_buf_get_name(0))
+          vim.cmd("LspStop")
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "GitConflictResolved",
+        callback = function()
+          vim.cmd("LspRestart")
+        end,
+      })
+    end,
+  },
+  {
     "tpope/vim-fugitive",
     config = function()
       Map("n", "<leader>gb", ":Telescope git_branches<CR>")
