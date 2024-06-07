f2812c0f776840a213eb57a0a5c6414bddf4507e
Valentin Kuznetsov <aimestereo@gmail.com>
2024-06-07 22:51:04 +0300

    nvim: .sh tabstop=2

--- nvim/.config/nvim/ftplugin/sh.lua
+++ nvim/.config/nvim/ftplugin/sh.lua
@@ -1,2 +1,5 @@
 vim.keymap.set("n", "<A-r>", ":.w !bash<CR>", { buffer = 0, desc = "Execute the current bash line" })
 vim.keymap.set("n", "<A-R>", ":w !bash<CR>", { buffer = 0, desc = "Execute bash file" })
+
+-- make tabs look like 2 spaces
+vim.opt.tabstop = 2
