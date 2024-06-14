ac1471995ec06a1596d048bde4889cadb66d07ae
Valentin Kuznetsov <aimestereo@gmail.com>
2024-06-14 23:03:33 +0300

    hammer: add menusearch

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -33,6 +33,9 @@ hyper.bindShiftKey("/", hs.fnutils.partial(sheet.toggle, sheet))
 -- Super Duper mode (hold s and d), Ah Fudge mode (hold a and f). Hold 5 seconds for help.
 require("keyboard") -- Load Hammerspoon bits from https://github.com/jasonrudolph/keyboard
 
+-- Menu item search.
+local menusearch = require("menusearch")
+hyper.bindKey("/", menusearch.toggle)
 
 local alert_sound = hs.sound.getByName("Tink")
 alert_sound:play()
