4c9c934def7c804a292f5ed2d7bfdbd91dad6584
Valentin Kuznetsov <aimestereo@gmail.com>
2024-11-11 16:48:18 +0300

    nvim: disable copilot

--- configs/nvim/.config/nvim/lua/plugins/ai.lua
+++ configs/nvim/.config/nvim/lua/plugins/ai.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    "github/copilot.vim",
-    config = function()
-      -- Set C-y to accept the suggestion
-      -- vim.keymap.set("i", "<C-y>", 'copilot#Accept("\\<CR>")', {
-      --   expr = true,
-      --   replace_keycodes = false,
-      -- })
-      -- vim.g.copilot_no_tab_map = true
-    end,
-  },
+  -- {
+  --   "github/copilot.vim",
+  --   config = function()
+  --     -- Set C-y to accept the suggestion
+  --     -- vim.keymap.set("i", "<C-y>", 'copilot#Accept("\\<CR>")', {
+  --     --   expr = true,
+  --     --   replace_keycodes = false,
+  --     -- })
+  --     -- vim.g.copilot_no_tab_map = true
+  --   end,
+  -- },
   {
     "jackMort/ChatGPT.nvim",
     event = "VeryLazy",
