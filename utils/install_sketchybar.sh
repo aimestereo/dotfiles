0a5c349c2801f4948f6a510c180a17291d5784e8
Valentin Kuznetsov <aimestereo@gmail.com>
2024-11-28 21:50:59 +0300

    sketchybar: localize

--- utils/install_sketchybar.sh
+++ utils/install_sketchybar.sh
@@ -1,5 +1,6 @@
 echo "Installing Dependencies"
 # Packages
+# Nix lua don't import local/relative packages
 brew install lua
 brew install switchaudio-osx
 brew install nowplaying-cli
@@ -12,14 +13,9 @@ brew install --cask sf-symbols
 brew install --cask font-sf-mono
 brew install --cask font-sf-pro
 
-curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o "$HOME"/Library/Fonts/sketchybar-app-font.ttf
 
 # SbarLua
 (git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
 
-echo "Cloning Config"
-git clone https://github.com/FelixKratz/dotfiles.git /tmp/dotfiles
-mv $HOME/.config/sketchybar $HOME/.config/sketchybar_backup
-mv /tmp/dotfiles/.config/sketchybar $HOME/.config/sketchybar
-rm -rf /tmp/dotfiles
 brew services restart sketchybar
