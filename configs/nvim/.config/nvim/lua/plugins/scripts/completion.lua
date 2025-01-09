d36f90f43db44fe6428d8c9ed1ed0ffec2d027c4
Valentin Kuznetsov <aimestereo@gmail.com>
2025-01-09 12:45:20 +0300

    nvim: use C-j/k in cmp

--- configs/nvim/.config/nvim/lua/plugins/scripts/completion.lua
+++ configs/nvim/.config/nvim/lua/plugins/scripts/completion.lua
@@ -12,8 +12,8 @@ cmp.setup({
 
   mapping = {
     -- Drop presets, set only keymaps that I use
-    ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-    ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete({}),
