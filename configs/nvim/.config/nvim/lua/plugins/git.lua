ce1b96612c45d9cf3917e3f891bbc6ec1f8d445e
Valentin Kuznetsov <aimestereo@gmail.com>
2025-04-01 22:47:12 +0300

    nvim: drop lazygit and fugitive

--- configs/nvim/.config/nvim/lua/plugins/git.lua
+++ configs/nvim/.config/nvim/lua/plugins/git.lua
@@ -48,38 +48,6 @@ return {
     end,
   },
   {
-    "tpope/vim-fugitive",
-    config = function()
-      -- Map("n", "<leader>gb", ":Telescope git_branches<CR>")
-      -- Map("n", "<leader>gB", ":Git blame<CR>")
-      Map("n", "<A-G>", ":0G<CR>", { desc = "Open [G]it fugitive in current pane." })
-    end,
-  },
-  {
-    "kdheepak/lazygit.nvim",
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-      { "<leader>lg", "<cmd>LazyGit<CR>", desc = "LazyGit" },
-    },
-    init = function()
-      require("telescope").load_extension("lazygit")
-      -- autocmd BufEnter * :lua require('lazygit.utils').project_root_dir()
-    end,
-  },
-  {
     "NeogitOrg/neogit",
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
