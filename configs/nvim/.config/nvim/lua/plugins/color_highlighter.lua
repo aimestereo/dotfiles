d5d11bfda50a17570a91afa0a5e162cbdc9ebb3e
Valentin Kuznetsov <aimestereo@gmail.com>
2025-11-17 22:29:16 +0300

    nvim: add color highlighter

---
+++ configs/nvim/.config/nvim/lua/plugins/color_highlighter.lua
@@ -0,0 +1,18 @@
+return {
+  "uga-rosa/ccc.nvim",
+  lazy = true,
+  event = { "BufRead" },
+  config = function()
+    local ccc = require("ccc")
+    local mapping = ccc.mapping
+
+    ccc.setup({
+      -- Your preferred settings
+      -- Example: enable highlighter
+      highlighter = {
+        auto_enable = true,
+        lsp = true,
+      },
+    })
+  end,
+}
