c4abe6385df20c72071938e40296a7f56b6d0c2a
Valentin Kuznetsov <aimestereo@gmail.com>
2025-11-19 17:23:48 +0300

    nvim: add spellcheck

--- configs/nvim/.config/nvim/lua/plugins/consts/servers.lua
+++ configs/nvim/.config/nvim/lua/plugins/consts/servers.lua
@@ -90,4 +90,38 @@ return {
       telemetry = { enable = false },
     },
   },
+
+  -- Spell checking language server configuration
+  harper_ls = {
+    ["harper-ls"] = {
+      userDictPath = "",
+      workspaceDictPath = "",
+      fileDictPath = "",
+      linters = {
+        SpellCheck = false,
+        SpelledNumbers = false,
+        AnA = true,
+        SentenceCapitalization = false,
+        UnclosedQuotes = true,
+        WrongQuotes = false,
+        LongSentences = true,
+        RepeatedWords = true,
+        Spaces = true,
+        Matcher = true,
+        CorrectNumberSuffix = true,
+      },
+      codeActions = {
+        ForceStable = false,
+      },
+      markdown = {
+        IgnoreLinkTitle = false,
+      },
+      diagnosticSeverity = "hint",
+      isolateEnglish = false,
+      dialect = "American",
+      maxFileLength = 120000,
+      ignoredLintsPath = "",
+      excludePatterns = {},
+    },
+  },
 }
